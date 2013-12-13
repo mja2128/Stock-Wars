077654778bd45d25af71e40d08d5d1227dce13ae
Michael Angerville <mja2128@gmail.com>
2013-12-12 20:10:23 -0500

    Adding Elston's code so far for the Stock class
    Contains Elston's Stock class and unit tests for it

---
+++ Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		{
+			String Tomika = "Tomika Industries";
+			Stocks stock1 = new Stocks(Tomika,100.00);
+			stock1.stockHistory(1);
+			System.out.println("Here is the intial price for" + stock1.getName());
+			System.out.println(stock1.getPrice());
+			
+			System.out.println();
+			System.out.print("Now we are gonna change the price of the Stock by setting the value to a different price");
+			System.out.println();
+			System.out.println();
+			stock1.setPrice(120.00);
+			stock1.stockHistory(2);
+			
+			System.out.println("Here is the price for" + stock1.getName());
+			System.out.println(stock1.getPrice());
+			
+			System.out.print("Now we are gonna change the price of the Stock Randomly");
+			System.out.println();
+			System.out.println();
+			stock1.randomPriceChange();
+			
+			System.out.println("Here is the price for" + stock1.getName());
+			System.out.println(stock1.getPrice());
+			}
+		}
+
+	}
+
+
